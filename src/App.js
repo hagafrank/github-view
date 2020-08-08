b6dbcedbd67877b47708f2c74da3a7ec6c3a094c
Frank Nkuyahaga <hagafrank@gmail.com>
2020-08-08 17:02:29 +0300

    WIP: Implement github user search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
-
-function handleSearchClick(searchTerm) {
-  fetchGitHubUser(searchTerm);
-}
-
-function fetchGitHubUser(name) {
-  let gitHubUrl = `https://api.github.com/users/${name}`;
-
-  fetch(gitHubUrl)
-    .then((res) => res.json())
-    .then((res) => console.log(res));
-}
+import SearchResults from "./components/SearchResults";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  function handleSearchClick() {
+    let gitHubUrl = `https://api.github.com/users/${searchTerm}`;
+
+    fetch(gitHubUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        searchResults.unshift(res);
+        setSearchResults(searchResults);
+        setSearchTerm("");
+      });
+  }
+
+  function removeResult(result) {
+    let index = searchResults.indexOf(result);
+    if (index > -1) {
+      searchResults.splice(index, 1);
+      setSearchResults(searchResults);
+    }
+  }
 
   return (
     <div className="App">
@@ -24,9 +34,13 @@ function App() {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button onClick={() => handleSearchClick(searchTerm)}>Search</button>
+          <button onClick={handleSearchClick}>Search</button>
         </div>
       </header>
+      <SearchResults
+        searchResults={searchResults}
+        removeResult={removeResult}
+      />
     </div>
   );
 }
